df606a46da26422a37c1350f8dfdd460a5d5f4fe
Mikhail Collins <kyleryker@gmail.com>
2017-03-08 12:56:58 -0700

    first http request mini project

--- controller.js
+++ controller.js
@@ -1,10 +1,28 @@
 angular.module('userProfiles').controller('MainController', function($scope, mainService) {
+$scope.currentPage = 1
 
   $scope.getUsers = function() {
-  	$scope.users = mainService.getUsers();
+    mainService.getUsers($scope.currentPage).then (function(response){
+      if (response.status === 200){
+      $scope.users = response.data.data;
+    }
+    });
   }
 
 
   $scope.getUsers();
-
+  $scope.prev = function (){
+    $scope.currentPage--;
+    if($scope.currentPage < 1){
+      $scope.currentPage = 1;
+    }
+      $scope.getUsers();
+  };
+  $scope.next = function (){
+    $scope.currentPage++;
+    if($scope.currentPage > $scope.total_pages){
+      $scope.currentPage = $scope.total_pages = 1;
+    }
+      $scope.getUsers();
+  };
 });
